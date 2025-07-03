3cd7876baf81ecb26d26dbe0ebbfaf276e973f83
Paulo Henrique <pauloh.2903@gmail.com>
2025-07-03 16:05:26 -0300

    atualização de arquivo.

---
+++ cypress/e2e/helpers.spec.js
@@ -0,0 +1,48 @@
+/// <reference types="cypress" />
+
+describe('Helpers...', () => {
+    it('Wrap', () => {
+        const obj = {nome: 'User', idade: 20}
+        expect(obj).to.have.property('nome')
+        cy.wrap(obj).should('have.property', 'nome')
+
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        //cy.get('#ForNome').then($el => {
+
+        const promise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(10)
+            }, 500)
+        })    
+            
+
+        cy.get('#buttonSimple').then(() => {
+            console.log('Encontrei primeiro botão')
+        })
+
+        cy.wrap(promise).then(ret => {
+            console.log(ret) // Deve imprimir 10
+        })
+
+        cy.get('#buttonList').then(() => {
+            console.log('Encontrei o segundo botão')
+        })
+
+    })
+})
+
+
+
+    it.only('Its...', () => {
+        const obj = {nome: 'User', idade: 20}
+        cy.wrap(obj).should('have.property', 'nome', 'User')
+        cy.wrap(obj).its('nome').should('be.equal', 'User')
+
+        const obj2 = {nome: 'User', idade: 20, endereco: {rua: 'dos bobos'}}
+        cy.wrap(obj2).its('endereco').should('have.property', 'rua')
+        cy.wrap(obj2).its('endereco').should('contain', 'bobos')
+        cy.wrap(obj2).its('endereco.rua').should('contain', 'bobos')
+
+         cy.visit('https://wcaquino.me/cypress/componentes.html')
+         cy.title().its('length').should('be.equal', 20)
+    })
