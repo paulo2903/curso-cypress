b77b65d3ad83d133c424692e8b72dc4b003357d3
Paulo Henrique <pauloh.2903@gmail.com>
2025-07-05 14:01:21 -0300

    Implementação do Invoke.

--- cypress/e2e/helpers.spec.js
+++ cypress/e2e/helpers.spec.js
@@ -29,20 +29,39 @@ describe('Helpers...', () => {
         })
 
     })
-})
 
 
 
-    it.only('Its...', () => {
+
+    it('Its...', () => {
         const obj = {nome: 'User', idade: 20}
         cy.wrap(obj).should('have.property', 'nome', 'User')
         cy.wrap(obj).its('nome').should('be.equal', 'User')
 
         const obj2 = {nome: 'User', idade: 20, endereco: {rua: 'dos bobos'}}
         cy.wrap(obj2).its('endereco').should('have.property', 'rua')
-        cy.wrap(obj2).its('endereco').should('contain', 'bobos')
+        cy.wrap(obj2).its('endereco').its('rua').should('contain', 'bobos')
         cy.wrap(obj2).its('endereco.rua').should('contain', 'bobos')
 
          cy.visit('https://wcaquino.me/cypress/componentes.html')
          cy.title().its('length').should('be.equal', 20)
+    })
+
+    it('Invoke...', () => {
+        const getValue = () => 1;
+        const soma = (a, b) => a + b;
+
+        //criando a função "fn" e usando o getValue como referência para a mesma
+        cy.wrap({ fn:getValue}).invoke('fn').should('be.equal', 1)  //quando invocado o 'fn', será exibido o resultado do getValue que é '1'.
+        cy.wrap({ fn:soma}).invoke('fn', 2, 5).should('be.equal', 7) 
+
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        cy.get('#formNome').invoke('val', 'Texto via invoke')
+        cy.window().invoke('alert', 'Dá pra ver?')
+        cy.get('#resultado')
+            .invoke('html', '<input type="button" value="hached!"/>')
+    })
+
+
+
     })
