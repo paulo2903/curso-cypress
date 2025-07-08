5fafde4c097650b0af1a031d54a23cef303e90d0
Paulo Henrique <pauloh.2903@gmail.com>
2025-07-08 16:38:22 -0300

    Refatoração do reaproveitamento do título.

--- cypress/e2e/basic.spec.js
+++ cypress/e2e/basic.spec.js
@@ -16,11 +16,25 @@ describe('Cypress basics', () => {
         cy.title()
             .should('be.equal', 'Campo de Treinamento')
             .and('contain', 'Campo')
+
             
-        
+        let syncTitle
+
         cy.title().then(title => {
             console.log(title)
+
+            cy.get('#formNome').type(title)
+
+            syncTitle = title
         })
+
+        cy.get('[data-cy="dataSobrenome"]').then($el => {
+            $el.val(syncTitle)
+        })
+
+        cy.get('#elementosForm\\:sugestoes').then($el => {   //onde há 2 pontos precisamos colocar 2 barras
+            $el.val(syncTitle)
+        })        
     })
 
 it('Should find and interact with an element', () => {
